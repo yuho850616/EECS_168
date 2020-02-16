1437624b673d54490e579299f04a5c5068c2ccb8
John Ho <yuho850616@gmail.com>
2020-02-15 22:35:58 -0600

    Add files via upload

---
+++ Ho-2911531-Lab-01/ex2/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main()
+{
+	std::cout<<"My name is Tsung Yu Ho\n";
+	std::cout<<"I am an CS major\n";
+	std::cout<<"My hobbies are:\n";
+	std::cout<<"\t Playing basketball\n";
+	std::cout<<"\t Playing League of Legends\n";
+	std::cout<<"\t Sleeping\n";
+	std::cout<<"\t Traveling\n";
+	std::cout<<"Goodbye\n";
+
+return(0);
+}
+
